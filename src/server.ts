5180b090d4eca2cf09b024d6b4c62f7e9d279fb9
Cavan Law <cavan.law@viooh.com>
2022-02-06 17:07:29 +0000

    update package.json

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import 'module-alias/register';
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
